61d7166def8e595281d07cf604fcbf47097aac2c
ARVIND\Arvind <asharv2011@gmail.com>
2023-04-24 12:00:26 +0800

    Scaffolding setup completed

---
+++ Assigment/Areas/Identity/Data/AssigmentUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Assigment.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the AssigmentUser class
+    public class AssigmentUser : IdentityUser
+    {
+    }
+}
---
+++ Assigment/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Assigment.Areas.Identity.Data;
+using Assigment.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Assigment.Areas.Identity.IdentityHostingStartup))]
+namespace Assigment.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AssigmentContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AssigmentContextConnection")));
+
+                services.AddDefaultIdentity<AssigmentUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AssigmentContext>();
+            });
+        }
+    }
+}
