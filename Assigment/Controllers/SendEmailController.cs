28a144fbe6bd6864545995666707908005220669
Khaimeng01 <102606978+Khaimeng01@users.noreply.github.com>
2023-05-08 20:48:38 +0800

    1. Email Services

---
+++ Assigment/Controllers/SendEmailController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Threading.Tasks;
+
+namespace Assigment.Controllers
+{
+    public class SendEmailController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task SendEmailUsingSendGrid(EmailAddress to)
+        {
+
+            var apiKey = "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";
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("TP055753@mail.apu.edu.my", "ADMIN");
+            var subject = "Reset Password";
+            //var to = new EmailAddress("lai.khaiming@gmail.com", "Khai Meng");
+            var plainTextContent = "Your Password Has Reset. Please login with your registered Email. Your new Password is (Ic Number)_Reset";
+            var htmlContent = "<strong>Your Password Has Reset. Please login with your registered Email. Your new Password is (Ic Number)_Reset</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+        }
+    }
+}
